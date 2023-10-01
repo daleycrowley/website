8cc7a7dc47855b125f65081a5a331141522fb5cb
daleycrowley <daleycrowley@yahoo.com>
2023-10-01 17:57:26 -0600

    update

--- js/daley.js
+++ js/daley.js
@@ -1,36 +1,28 @@
 (function() {
-    function showHide(parent, showingChild, hiddingChild) {
-        document.getElementById(parent).addEventListener('mouseover', function(event) {
-            document.getElementById(hiddingChild).classList.add('showHobby');
-            document.getElementById(hiddingChild).classList.remove('hideHobby');
-            document.getElementById(showingChild).classList.add('hideHobby');
-            document.getElementById(showingChild).classList.remove('showHobby');
-        })
-
-        document.getElementById(parent).addEventListener('mouseout', function(event) {
-            document.getElementById(hiddingChild).classList.remove('showHobby');
-            document.getElementById(hiddingChild).classList.add('hideHobby');
-            document.getElementById(showingChild).classList.remove('hideHobby');
-            document.getElementById(showingChild).classList.add('showHobby');
-        })
-    }
-    showHide('nailsParent', 'nailsShowing', 'nailsReveal')
-    showHide('vintageParent', 'vintageShowing', 'vintageReveal')
-    showHide('embroideryParent', 'embroideryShowing', 'embroideryReveal')
+    
 
     // collapsible stuff
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
+    // var coll = document.getElementsByClassName("collapsible");
+    // var i;
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("activePortfolio");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
+    // for (i = 0; i < coll.length; i++) {
+    //     coll[i].addEventListener("click", function() {
+    //         this.classList.toggle("activePortfolio");
+    //         var content = this.nextElementSibling;
+    //         if (content.style.maxHeight) {
+    //             content.style.maxHeight = null;
+    //         } else {
+    //             content.style.maxHeight = content.scrollHeight + "px";
+    //         }
+    //     });
+    // }
+
+
+    document.getElementById('nailsReveal').onclick = function() {
+        window.open("https://www.instagram.com/dailydaleynails/", "_blank");
+    }
+
+    document.getElementById('vintageReveal').onclick = function() {
+        window.open("https://www.etsy.com/shop/DaleyVintage?ele=shop_open", "_blank");
     }
 })()
